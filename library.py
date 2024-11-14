36f695035cc26280291376400e4a89787986a79a
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2024-11-14 10:40:36 +0530

    Create library.py

---
+++ library.py
@@ -0,0 +1,12 @@
+import json
+from notifier_botty import login_to_chegg, refresh_chegg, telegram_bot_sendtext
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime, timedelta
+import pytz
+import requests
+import multiprocessing
+import base64
+from io import StringIO

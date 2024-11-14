3c8632c6e4224ab7cd5ac068a878295d2453c6e9
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2024-11-14 10:48:08 +0530

    Update library.py

--- library.py
+++ library.py
@@ -1,5 +1,5 @@
 import json
-from notifier_botty import login_to_chegg, refresh_chegg, telegram_bot_sendtext
+from notifier_botty import login_to_chegg, refresh_chegg, telegram_both_sendtext
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
